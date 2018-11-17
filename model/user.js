48d5c52324e027e5a21b880738d4ef2fa387ca18
Hadia Hameed <hadiahameed@Hadias-MacBook-Pro.local>
2018-11-17 17:48:19 -0500

    mongoose for database

--- model/user.js
+++ model/user.js
@@ -1,6 +1,9 @@
-const Model = require('./model')
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt')
 
-module.exports = async () => {
+//const Model = require('./model')
+
+/*module.exports = async () => {
     let User = await Model('user', {
         name: String,
         email: String,
@@ -17,4 +20,64 @@ module.exports = async () => {
     })
 
     return User
-}
+}*/
+
+var UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    validated: {
+        type: Boolean
+    },
+    validation_code: {
+        type: Number
+    }
+});
+
+    UserSchema.statics.authenticate = function (email,password,callback) {
+        UserSchema.findOne({ email:email })
+            .exec(function (err,user) {
+                if (err) {
+                    return callback(err)
+                } else if (!user) {
+                    var err = new Error('User not found.');
+                    err.status = 401;
+                    return callback(err);
+                }
+                bcrypt.compare(password, user.password, function (err, result) {
+                    if (result === true) {
+                        return callback(null, user);
+
+                    }else {
+                        return callback();
+                    }
+                })
+            });
+    }
+
+UserSchema.pre('save',function (next){
+    var user = this;
+    bcrypt.hash(user.password,10,function(err,hash){
+        if (err){
+            return next(err);
+        }
+        user.password = hash;
+        next();
+    })
+});
+
+var User = mongoose.model('User', UserSchema);
+module.exports = User;
