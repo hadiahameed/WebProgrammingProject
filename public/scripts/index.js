c4452e201944b575e069da880a8badc796f62c18
Hadia Hameed <hadiahameed@Hadias-MacBook-Pro.local>
2018-11-17 19:26:41 -0500

    User registration form updated, confirm password alert added

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,9 +1,29 @@
 $(document).ready(function () {
-    $('#newuser').on('submit', function() {
+    /*$('#newuser').on('submit', function() {
         let g_response = $('#g-recaptcha-response').val()
         if (!g_response) {
             $.alert('Please check the captcha box to proceed to the destination page.');
             return false
         }
+    })*/
+    $('#submitButton').click(function(e) {
+        var password = $("#password").val();
+        var confirmPassword = $("#confirmPassword").val();
+        let g_response = $('#g-recaptcha-response').val();
+        if (password == confirmPassword){
+            if (!g_response) {
+                $.alert('Please check the captcha box to proceed to the destination page.');
+                return false
+            }
+            else {
+                return true;
+            }
+        }
+        else {
+            $.alert(`Passwords don't match.`);
+            return false
+        }
     })
+
 })
+    
