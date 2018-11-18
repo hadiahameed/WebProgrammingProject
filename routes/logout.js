29dec809a005757c4d3cc72986bee3cbba1f9d35
VidyaMaiya <vid04star@gmail.com>
2018-11-18 07:16:48 -0500

    Added comments

--- routes/logout.js
+++ routes/logout.js
@@ -2,6 +2,10 @@ const express=require("express");
 const router=express.Router();
 
 router.post("/",(req,res) => {
+    /*
+    * Clear the cookie while logging out
+    * Render the home page with some message
+    */
     if(req.cookies.AuthCookie)
         res.clearCookie('AuthCookie');
         res.render("page/index",{error:"You've successfully logged out!"})
