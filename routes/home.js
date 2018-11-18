7decac50f1d870fdb4447e43095cf760a7f8929f
VidyaMaiya <vid04star@gmail.com>
2018-11-17 20:21:28 -0500

    Adding Cookie check in home.js route

--- routes/home.js
+++ routes/home.js
@@ -4,8 +4,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render("pages/index", { title: 'Log in' });
+  if(!req.cookies.AuthCookie)
+    res.render("pages/index", { title: 'Log in' });
+  else
   // if logged in: "pages.home"
+    res.render("pages/home",{title:"Welcome to the home page"});
 });
 
 module.exports = router;
