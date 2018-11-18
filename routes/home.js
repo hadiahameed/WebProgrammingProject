1ea542772f069e381aa838db7e4492d484708bba
VidyaMaiya <vid04star@gmail.com>
2018-11-18 07:20:20 -0500

    Added comments

--- routes/home.js
+++ routes/home.js
@@ -4,10 +4,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  /* Check if the cookie named 'AuthCookie' exists
+   * If exits navigate to home page
+   * If not render the login page
+   */
   if(!req.cookies.AuthCookie)
     res.render("pages/index", { title: 'Log in' });
   else
-  // if logged in: "pages.home"
+    //if logged in: "pages.home"
     res.render("pages/home",{title:"Welcome to the home page"});
 });
 
