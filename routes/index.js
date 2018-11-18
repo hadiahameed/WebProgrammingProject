81ce574cc72f9a2fdfba6e11bdcea5735be3de1a
hadiahameed <hadiahameeduet@gmail.com>
2018-11-17 20:18:25 -0500

    books router added in /routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const homeRouter = require('./home')
 const usersRouter = require('./users');
+const booksRouter = require('./books');
 const vlaidateRouter = require('./validate')
 
 module.exports = app => {
     app.use('/', homeRouter)
     app.use('/users', usersRouter)
     app.use('/validate', vlaidateRouter)
+    app.use('/books', booksRouter)
 }
