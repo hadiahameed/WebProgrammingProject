3fd697743641f0dc2b95f93dcf13fd99f206179a
Sanne <sanneswagerman@MBP-van-Sanne.home>
2018-11-22 15:35:36 -0500

    Updated handlebars + provided a short description of each handlebar file

--- routes/index.js
+++ routes/index.js
@@ -7,17 +7,22 @@ const loginRouter = require('./login');
 const userProfileRouter = require('./userProfile');
 
 module.exports = app => {
-    app.use('/', homeRouter)
-    app.use('/users', usersRouter)
-    app.use('/validate', vlaidateRouter)
-    app.use('/books', booksRouter)
-    app.use('/login',loginRouter);
-    //app.use('/logout',logoutRouter);
-    app.use('/userProfileRouter',userProfileRouter);
-    app.use('*', async (req, res, next) => {
-        res.status(404)
-        res.render('pages/error', {
-            error: "Could not find this page"
-        })
-    })
+    // app.use('/', homeRouter)
+    // app.use('/users', usersRouter)
+    // app.use('/validate', vlaidateRouter)
+    // app.use('/books', booksRouter)
+    // app.use('/login',loginRouter);
+    // //app.use('/logout',logoutRouter);
+    // app.use('/userProfileRouter',userProfileRouter);
+    // app.use('*', async (req, res, next) => {
+    //     res.status(404)
+    //     res.render('pages/error', {
+    //         error: "Could not find this page"
+    //     })
+    // })
+
+    // // Please ignore this, this is just for easy testing purposes for the handlebars
+    // app.get('/', function(req, res, next) {
+    //     res.render("books/books", { firstName: 'Sanne' });
+    // })
 }
