ba362eb9a2e07addba84b2935baf5929067163e0
hadiahameed <hadiahameeduet@gmail.com>
2018-11-28 13:12:07 -0500

    userProfile

--- routes/userProfile.js
+++ routes/userProfile.js
@@ -11,12 +11,7 @@ router.get("/", async(req,res) => {
     res.render("user/userProfile",{
                         user: user.props,
                         "title"     : "You're viewing user profile page",
-<<<<<<< HEAD
-                        "firstName" : "Sanne",
-                        loggedIn: true    
-=======
                         "firstName" : req.user.firstname,    
->>>>>>> 89c24a4e20f3cfc7b830a2d3594e5cf6aaeddf7c
                         // "firstName" : userData.firstname,
                             // "lastName"   : userData.lastname
                             });
