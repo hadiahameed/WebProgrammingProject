50670db61adcca7c52d4dffc8883130cf50cce90
VidyaMaiya <vid04star@gmail.com>
2018-11-28 13:17:12 -0500

    Committing again all files

--- routes/userProfile.js
+++ routes/userProfile.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const userModel = require('../model/user')
 const cookieMiddleware = require("../middlewares/validateCookie");
 
 router.get("/", async(req,res) => {
-    // cookieMiddleware, 
-   
-
+    // cookieMiddleware,
+    let User = await userModel();
+    let user = await User.getById(req.user._id);
+    
     res.render("user/userProfile",{
-                        user: req.user,
+                        user: user.props,
                         "title"     : "You're viewing user profile page",
                         "firstName" : req.user.firstname,    
                         // "firstName" : userData.firstname,
