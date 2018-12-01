b8113fff645ac3622aa6cc36f6df9a7353a5b64a
silas <i@silas.hk>
2018-11-30 20:45:42 -0500

    move passport to middleware folder

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const layouts = require('handlebars-layouts');
 const logger = require('morgan');
 const router = require('./routes');
 //const passport = require('passport');
-const passport = require('./routes/passport');
+const passport = require('./middlewares/passport');
 const expressValidator = require('express-validator');
 const flash = require('connect-flash');
 const session = require('express-session');
@@ -51,20 +51,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page 
-  res.status(err.status || 500);
-  res.render("pages/error");
-  // ^^ this should render whatever page and pass in error: true
-});
-
 //Express session
 app.use(session({
   secret:'secret',
@@ -106,5 +92,18 @@ router(app)
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page 
+  res.status(err.status || 500);
+  res.render("pages/error");
+  // ^^ this should render whatever page and pass in error: true
+});
+
 
 module.exports = app;
---
+++ middlewares/passport.js
@@ -0,0 +1,48 @@
+var LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt')
+const userModel = require('../model/user')
+const passport = require('passport');
+
+
+// passport session setup, required for persistent login sessions
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+//Attention: you can also use the same method body as serializeUser. remove user by id is not nesssary.
+passport.deserializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.use('local', new LocalStrategy({
+    usernameField: 'user-name',
+    passwordField: 'user-password'
+}, async function (username, password, done) {
+    let user = null;
+    let User = await userModel()
+    user = await User.getBy({
+        username
+    });
+    //console.log(user);
+    if (user.length == 0) {
+        //console.log("No user")
+        // return done(null,false, {message : 'Incorrect username'});
+        return done(new Error('Incorrect username'), false);
+    }
+    user = user[0]
+    if (!user.validated) {
+        // return done(null,false,{message : 'Please validate your account'})
+        return done(new Error('Please validate your account'), false)
+    }
+    //console.log("No error")
+
+    if (!await bcrypt.compare(password, user.password)) {
+        console.log("Not matched")
+        // return done(null, false, {message:'Password do not match'});
+        return done({
+            message: 'Password do not match'
+        }, false);
+    }
+
+    return done(null, user);
+}));
+module.exports = passport
