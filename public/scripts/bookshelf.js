af4c09f6565b6317d565997baa2b74b635ff1782
silas <i@silas.hk>
2018-12-02 17:42:05 -0500

    move scripts to corresponding files

---
+++ public/scripts/bookshelf.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+    $(".btnExc").click(async function () {
+        try {
+            let result = await axios.delete('/bookshelves', {
+                data: {bookshelf: $(this).parent().attr('id')}
+              });
+            location.reload(true)    
+        }
+        catch(e) {
+            console.log(e)
+        }
+    })
+    
+})
---
+++ public/scripts/bookshelves.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    
+    $(".btnEdit").click(function(){
+        window.location.href = '/books/new'
+    })
+
+    $(".btnExc").click(async function () {
+        try {
+            let result = await axios.delete('/bookshelves', {
+                data: {bookshelf: $(this).parent().attr('id')}
+              });
+            location.reload(true)    
+        }
+        catch(e) {
+            console.log(e)
+        }
+    })
+    
+})
